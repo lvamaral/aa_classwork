29f5b804a2db59228662f92be5104f97c2c5d97b
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-04-25 16:22:23 -0700

    Update ControllerBase Class

--- W5D2/RailsLite/lib/controller_base.rb
+++ W5D2/RailsLite/lib/controller_base.rb
@@ -7,23 +7,38 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
-    @req = req
-    @res = res
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
+    @@protect_from_forgery ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "double render error" if already_built_response?
+
+    @res.status = 302
+    @res["Location"] = url
+
+    @already_built_response = true
+
+    session.store_session(@res)
+    flash.store_flash(@res)
+
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+
   end
 
   # use ERB and binding to evaluate templates
